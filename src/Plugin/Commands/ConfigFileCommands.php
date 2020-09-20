662c3e3b7c901bcf488a26fc22d9748730304411
Pieter Frenssen <pieter@frenssen.be>
2020-09-20 14:19:16 +0300

    Create the skeleton for the config file commands.

---
+++ src/Plugin/Commands/ConfigFileCommands.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace CodeLab\Robo\Plugin\Commands;
+
+class ConfigFileCommands extends \Robo\Tasks
+{
+    /**
+     * @command behat:generate-config
+     */
+    public function behatGenerateConfig(): void { }
+
+}
